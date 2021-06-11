2455eb2c293ac5860d83954f551caa37f21bd475
Lucas Lamar <lucass.lamar@gmail.com>
2021-06-11 15:31:05 -0300

    feat: valida campo cidade

--- src/app/config/routes.js
+++ src/app/config/routes.js
@@ -1,11 +1,13 @@
 import { Router } from 'express';
 
+import NewCity from '@/app/utils/validators/City/new';
+
 import CityController from '@/app/controllers/CityController';
 
 const routes = new Router();
 
 
 routes.get('/api/v1/city', CityController.index);
-routes.post('/api/v1/city', CityController.create);
+routes.post('/api/v1/city', [NewCity], CityController.create);
 
 export default routes;
--- src/app/utils/validators/City/new.js
+++ src/app/utils/validators/City/new.js
@@ -2,7 +2,13 @@ import yup from '../yupLocale';
 
 export default async (req, res, next) => {
     try {
-        const schema = yup.object().shape({});
+        const schema = yup.object().shape({
+          firstName: yup
+                .string()
+                .min(3)
+                .matches(/^[a-záàâãéèêíïóôõöúçñ ]+$/i)
+                .required('Nome é obrigatório!'),
+        });
         await schema.validate(req.body, { abortEarly: false });
         return next();
     } catch (error) {
